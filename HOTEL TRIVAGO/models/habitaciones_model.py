2fb2a549be8a4b5397e426f4b51c82c277163912
Israel-Rodri <israel5302@gmail.com>
2025-06-05 23:54:47 -0400

    agregar

--- HOTEL TRIVAGO/models/habitaciones_model.py
+++ HOTEL TRIVAGO/models/habitaciones_model.py
@@ -29,8 +29,38 @@ class ConexionDB:
             conf = self.obtener_datos(consulta)
             if conf != None:
                 try:
-                    consulta = 'INSERT INTO habitacion VALUES(%s, %s, "%s", "%s", %s, "%s")'
-                    self.ejecutar_consulta(consulta, (cod, num, tip, cap, pre, stat))
+                    consulta = 'INSERT INTO habitacion VALUES(%s, %s, %s, %s, %s, %s)'
+                    #Conversión de tipo de habitación
+                    if tip == 'normal':
+                        tipFormat = 'N'
+                    elif tip == 'ejecutiva':
+                        tipFormat = 'E'
+                    elif tip == 'suite':
+                        tipFormat = 'S'
+                    else:
+                        e = 'Tipo de la habitación no válido'
+                        return e
+                    #Conversión de capacidad de habitación
+                    if cap == 'individual':
+                        capFormat = 'I'
+                    elif cap == 'matrimonial':
+                        capFormat = 'M'
+                    elif cap == 'doble':
+                        capFormat = 'D'
+                    elif cap == 'triple':
+                        capFormat = 'T'
+                    else:
+                        e = 'Capacidad de la habitación no válida'
+                        return e
+                    #Conversión de status de habitación
+                    if stat == 'disponible':
+                        statFormat = 'D'
+                    elif stat == 'ocupada':
+                        statFormat = 'O'
+                    else:
+                        e = 'Status de la habitación no válida'
+                    print(tipFormat, capFormat, statFormat)
+                    self.ejecutar_consulta(consulta, (cod, num, tipFormat, capFormat, pre, statFormat))
                     self.cerrar_conexion()
                     result = True
                     return result
